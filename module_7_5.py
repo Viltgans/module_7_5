4a41e17e07ff0ad9aecd44a0011f778b65645247
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-09-28 19:23:45 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+directory = os.getcwd()
+directory2 = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(directory, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(directory)
+
+print(f'Обнаружен файл: {file}\n'
+      f'Путь: {filepath}\n'
+      f'Размер: {filesize} байт\n'
+      f'Время изменения: {formatted_time}\n'
+      f'Родительская директория: {parent_dir}\n')
+
+for root, dirs, files in os.walk(directory2):
+    for file in files:
+        filepath = os.path.join(directory2, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(directory2)
+
+print(f'Обнаружен файл: {file}\n'
+      f'Путь: {filepath}\n'
+      f'Размер: {filesize} байт\n'
+      f'Время изменения: {formatted_time}\n'
+      f'Родительская директория: {parent_dir}\n')
